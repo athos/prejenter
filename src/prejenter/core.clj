472d22b1691db2780d9650dbb3e7b1b08c519c29
OHTA Shogo <athos0220@gmail.com>
2018-05-19 22:53:45 +0900

    Make slide size configurable

--- src/prejenter/core.clj
+++ src/prejenter/core.clj
@@ -30,9 +30,9 @@
 (defn render-coll [ctx tree]
   (reduce render ctx tree))
 
-(defmethod render* :slide [{:keys [g] :as ctx} [_ _ & body]]
+(defmethod render* :slide [{:keys [g width height] :as ctx} [_ _ & body]]
   (.setColor g Color/WHITE)
-  (.fillRect g 0 0 200 200)
+  (.fillRect g 0 0 width height)
   (render-coll ctx body))
 
 (defmethod render* :title [{:keys [g] :as ctx} [_ _ title]]
@@ -44,8 +44,8 @@
   (doseq [[i item] (map-indexed vector items)]
     (.drawString g (str "- " item) 10 (+ 50 (* i 10)))))
 
-(defn gen-image [ctx x]
-  (let [img (BufferedImage. 200 200 BufferedImage/TYPE_3BYTE_BGR)
+(defn gen-image [{:keys [width height] :as ctx} x]
+  (let [img (BufferedImage. width height BufferedImage/TYPE_3BYTE_BGR)
         g (.getGraphics img)
         ctx (assoc ctx :g g)]
     (render ctx x)
@@ -53,6 +53,9 @@
     img))
 
 (comment
+
+  (def context
+    {:width 640 :height 480})
 
   (def slide
     [:slide
@@ -62,7 +65,7 @@
       "bar"
       "baz"]])
 
-  (let [img (gen-image {} slide)]
+  (let [img (gen-image context slide)]
     (ImageIO/write img "png" (File. "/path/to/image.png")))
 
   )
