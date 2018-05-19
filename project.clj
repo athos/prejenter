73d45e5cac8adc89458c71709ca73ff89c6cde9e
OHTA Shogo <athos0220@gmail.com>
2018-05-19 23:44:24 +0900

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prejenter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Presentation framework for Clojurian"
+  :url "https://github.com/athos/prejenter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
