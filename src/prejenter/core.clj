0462a49785ac0ea66da3a6ebac83321beb68e41c
OHTA Shogo <athos0220@gmail.com>
2018-05-19 10:29:54 +0900

    first commit

---
+++ src/prejenter/core.clj
@@ -0,0 +1,6 @@
+(ns prejenter.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/prejenter/core_test.clj
@@ -0,0 +1,7 @@
+(ns prejenter.core-test
+  (:require [clojure.test :refer :all]
+            [prejenter.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
