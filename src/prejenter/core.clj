44592541d48bccc250f13e64a3e2c2040cce9806
OHTA Shogo <athos0220@gmail.com>
2018-05-19 13:04:13 +0900

    Say "Hello World" with ImageIO

--- src/prejenter/core.clj
+++ src/prejenter/core.clj
@@ -1,6 +1,21 @@
-(ns prejenter.core)
+(ns prejenter.core
+  (:import [java.awt Color]
+           [java.awt.image BufferedImage]
+           [java.io File]
+           [javax.imageio ImageIO]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn hello-world [^File dst]
+  (let [img (BufferedImage. 200 200 BufferedImage/TYPE_3BYTE_BGR)
+        g (.getGraphics img)]
+    (.setColor g Color/WHITE)
+    (.fillRect g 0 0 200 200)
+    (.setColor g Color/BLACK)
+    (.drawString g "Hello, World!" 100 100)
+    (.dispose g)
+    (ImageIO/write img "png" dst)))
+
+(comment
+
+  (hello-world (File. "/path/to/image.png"))
+
+  )
