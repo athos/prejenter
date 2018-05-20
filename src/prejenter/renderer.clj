90a88a5446b40d1346d606192f02e6b6049ea5f1
OHTA Shogo <athos0220@gmail.com>
2018-05-20 20:00:49 +0900

    Position texts automatically

--- src/prejenter/renderer.clj
+++ src/prejenter/renderer.clj
@@ -1,5 +1,5 @@
 (ns prejenter.renderer
-  (:import [java.awt Color Font]))
+  (:import [java.awt Color Graphics2D Font]))
 
 (defmulti render* (fn [ctx elem] (first elem)))
 
@@ -38,7 +38,7 @@
        (get ctx attr-name)
        default-value)))
 
-(defn render-text [{:keys [g] :as ctx} attrs x y text]
+(defn- set-font [{:keys [g] :as ctx} attrs]
   (let [font-size (attr-value ctx attrs :font-size)
         font-family (attr-value ctx attrs :font-family)
         font-style (get {:normal Font/PLAIN :italic Font/ITALIC}
@@ -47,23 +47,37 @@
         font-weight (get {:normal Font/PLAIN :bold Font/BOLD}
                          (attr-value ctx attrs :font-weight)
                          Font/PLAIN)
-        font (Font. font-family (bit-or font-style font-weight) font-size)
-        color (attr-value ctx attrs :color Color/BLACK)]
-    (.setFont g font)
-    (.setColor g color)
-    (.drawString g text x y)
-    ctx))
+        font (Font. font-family (bit-or font-style font-weight) font-size)]
+    (.setFont g font)))
+
+(defn- text-metrics [^Graphics2D g ^String text]
+  (let [fm (.getFontMetrics g)
+        lm (.getLineMetrics fm text g)]
+    {:width (.stringWidth fm text)
+     :height (.getHeight lm)
+     :ascent (.getAscent lm)}))
+
+(defn render-text [{:keys [g] :as ctx} attrs text]
+  (.setColor g (attr-value ctx attrs :color Color/BLACK))
+  (set-font ctx attrs)
+  (let [{:keys [::current-x ::current-y]} ctx
+        {:keys [width height ascent]} (text-metrics g text)]
+    (.drawString g text current-x (int (+ current-y ascent)))
+    (-> ctx
+        (update ::current-x + width)
+        (update ::current-y + height))))
 
 (defmethod render* :slide [{:keys [g] :as ctx} [_ _ & body]]
-  (.setColor g (:background-color ctx))
-  (.fillRect g 0 0 (:width ctx) (:height ctx))
-  (render-coll ctx body))
+  (let [ctx (assoc ctx ::current-x 0 ::current-y 0)]
+    (.setColor g (:background-color ctx))
+    (.fillRect g 0 0 (:width ctx) (:height ctx))
+    (render-coll ctx body)))
 
 (defmethod render* :title [{:keys [g] :as ctx} [_ attrs title]]
-  (render-text ctx attrs 10 50 title))
+  (render-text ctx attrs title))
 
 (defmethod render* :items [{:keys [g] :as ctx} [_ attrs & items]]
   (reduce (fn [ctx [i item]]
-            (render-text ctx attrs 10 (+ 100 (* i 30)) (str "- " item)))
+            (render-text ctx attrs (str "- " item)))
           ctx
           (map-indexed vector items)))
