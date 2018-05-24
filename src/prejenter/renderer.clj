4f3e04d53dc2b55b94f499710145cca8d957c990
OHTA Shogo <athos0220@gmail.com>
2018-05-25 00:38:30 +0900

    Fix reflection warnings

--- src/prejenter/renderer.clj
+++ src/prejenter/renderer.clj
@@ -2,6 +2,8 @@
   (:require [prejenter.layout :as layout])
   (:import [java.awt Color Graphics2D]))
 
+(set! *warn-on-reflection* true)
+
 (defmulti render* (fn [ctx elem] (:tag elem)))
 
 (defn render [ctx elem]
@@ -18,19 +20,23 @@
                 ctx (move-to ctx x y)]]
     (render ctx elem)))
 
-(defn render-text [{:keys [g] :as ctx} attrs text]
+(defn render-text [{:keys [^Graphics2D g] :as ctx} attrs ^String text]
   (.setColor g (::layout/color attrs))
   (.setFont g (::layout/font attrs))
   (let [{::keys [current-x current-y]} ctx
         {::layout/keys [width height ascent]} attrs]
-    (.drawString g text current-x (int (+ current-y ascent)))))
+    (.drawString g text (int current-x) (int (+ current-y ascent)))))
 
 (defn render-image [{:keys [g] :as ctx} {::layout/keys [image width height]}]
-  (.drawImage g image (::current-x ctx) (::current-y ctx) width height nil))
+  (.drawImage ^Graphics2D g image
+              (::current-x ctx)
+              (::current-y ctx)
+              width height nil))
 
-(defmethod render* :slide [{:keys [g width height] :as ctx} {:keys [attrs body]}]
+(defmethod render* :slide [{:keys [width height] :as ctx} {:keys [attrs body]}]
   (let [{:keys [padding-top padding-left]} attrs
-        ctx (assoc ctx ::current-x padding-left ::current-y padding-top)]
+        ctx (assoc ctx ::current-x padding-left ::current-y padding-top)
+        ^Graphics2D g (:g ctx)]
     (.setColor g (:background-color ctx))
     (.fillRect g 0 0 width height)
     (render-elems ctx body)))
