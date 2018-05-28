644531b7c1f2796faf6628b1955021c86b43180a
OHTA Shogo <athos0220@gmail.com>
2018-05-28 22:29:59 +0900

    Add REPL utils

--- src/prejenter/core.clj
+++ src/prejenter/core.clj
@@ -1,61 +1,37 @@
 (ns prejenter.core
-  (:require [prejenter.layout :as layout]
-            [prejenter.renderer :as renderer])
-  (:import [java.awt Graphics2D RenderingHints]
-           [java.awt.image BufferedImage]))
+  (:require [prejenter.agent :as agent]
+            [prejenter.agent.images :as images]))
 
-(set! *warn-on-reflection* true)
+(def ^:private current-agent (atom nil))
 
-(defn- enable-antialiasing [^Graphics2D g]
-  (doto g
-    (.setRenderingHint RenderingHints/KEY_ANTIALIASING
-                       RenderingHints/VALUE_ANTIALIAS_ON)
-    (.setRenderingHint RenderingHints/KEY_TEXT_ANTIALIASING
-                       RenderingHints/VALUE_TEXT_ANTIALIAS_ON)))
+(defn- find-name [ns name]
+  (let [kname (keyword (clojure.core/name name))]
+    (or (some->> (vals (ns-publics ns))
+                 (filter #(get (meta %) kname))
+                 first
+                 deref)
+        (some-> (ns-resolve ns name) deref))))
 
-(defn gen-image [{:keys [width height] :as ctx} x]
-  (let [img (BufferedImage. width height BufferedImage/TYPE_3BYTE_BGR)
-        g (.getGraphics img)
-        ctx (assoc ctx :g g)]
-    (enable-antialiasing g)
-    (renderer/render ctx (layout/layout ctx x))
-    (.dispose g)
-    img))
+(defn start! [& {:keys [ns context slides agent]}]
+  (when ns
+    (require ns :reload))
+  (let [context (or (some-> ns (find-name 'context))
+                    context)
+        slides (or (some-> ns (find-name 'slides))
+                   slides)
+        agent (or agent (images/make-images-agent context slides))]
+    (reset! current-agent agent)
+    (agent/start! agent)))
 
-(comment
+(defn next!
+  ([] (next! 1))
+  ([n]
+   (agent/next-page! @current-agent n)))
 
-  (import '[java.awt Color]
-          '[java.awt.image]
-          '[java.io File]
-          '[javax.imageio ImageIO])
+(defn prev!
+  ([] (prev! 1))
+  ([n]
+   (agent/prev-page! @current-agent n)))
 
-  (def context
-    {:width 640 :height 480
-     :padding 20
-     :font-family "Gill Sans"
-     :font-size 30
-     :color Color/BLACK
-     :background-color Color/WHITE})
-
-  (def slide
-    [:slide
-     [:title
-      {:font-style :italic :font-weight :bold
-       :padding-left 10 :padding-bottom 10
-       :height 75 :text-align :center :vertical-align :middle}
-      [:inline "Hello, " [:text {:color Color/RED :font-size 40} "World!"]]]
-     [:items {:color Color/BLUE}
-      [:text {:color Color/RED} "foo"]
-      "bar"
-      "baz"]
-     [:lines {:color Color/GREEN}
-      "hoge"
-      "fuga"
-      "piyo"]
-     [:image {:src (ImageIO/read (File. "/path/to/clojure_logo.png"))
-              :bottom 10 :right 10 :width 96 :height 96}]])
-
-  (let [img (gen-image context slide)]
-    (ImageIO/write img "png" (File. "/path/to/image.png")))
-
-  )
+(defn goto! [page]
+  (agent/prev-page! @current-agent page))
