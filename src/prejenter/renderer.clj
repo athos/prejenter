6bad98dacbef4750c29908380990a5ece558ef7e
OHTA Shogo <athos0220@gmail.com>
2018-05-30 00:22:49 +0900

    Paint background with the color specified as :background-color

--- src/prejenter/renderer.clj
+++ src/prejenter/renderer.clj
@@ -17,7 +17,19 @@
       (update ::current-x + x)
       (update ::current-y + y)))
 
-(defn render-elements [ctx elems]
+(defn fill-rect [{:keys [^Graphics2D g]} x y width height color]
+  (.setColor g color)
+  (.fillRect g x y width height))
+
+(defn render-background [ctx attrs]
+  (when-let [color (:background-color attrs)]
+    (let [{::layout/keys [width height padding-top padding-left]} attrs
+          x (- (::current-x ctx) padding-left)
+          y (- (::current-y ctx) padding-top)]
+      (fill-rect ctx x y width height color))))
+
+(defn render-elements [ctx attrs elems]
+  (render-background ctx attrs)
   (doseq [{:keys [attrs] :as elem} elems
           :let [{::layout/keys [x y]} attrs
                 ctx (move-to ctx x y)]]
@@ -43,29 +55,29 @@
 
 (defmethod render-element :slide [{:keys [width height] :as ctx} {:keys [attrs body]}]
   (let [{::layout/keys [padding-top padding-left]} attrs
-        ctx (assoc ctx ::current-x padding-left ::current-y padding-top)
-        ^Graphics2D g (:g ctx)]
-    (.setColor g (:background-color ctx))
-    (.fillRect g 0 0 width height)
-    (render-elements ctx body)))
+        ctx (assoc ctx ::current-x padding-left ::current-y padding-top)]
+    (fill-rect ctx 0 0 width height (:background-color ctx))
+    (render-elements ctx attrs body)))
 
 (defmethod render-element :text [ctx {:keys [attrs body]}]
+  (render-background ctx attrs)
   (render-text ctx attrs body))
 
 (defmethod render-element :image [ctx {:keys [attrs]}]
+  (render-background ctx attrs)
   (render-image ctx attrs))
 
 (defmethod render-element :title [ctx {:keys [attrs body]}]
-  (render-elements ctx body))
+  (render-elements ctx attrs body))
 
 (defmethod render-element :inline [ctx {:keys [attrs body]}]
-  (render-elements ctx body))
+  (render-elements ctx attrs body))
 
 (defmethod render-element :block [ctx {:keys [attrs body]}]
-  (render-elements ctx body))
+  (render-elements ctx attrs body))
 
 (defmethod render-element :lines [ctx {:keys [attrs body]}]
-  (render-elements ctx body))
+  (render-elements ctx attrs body))
 
 (defmethod render-element :items [ctx {:keys [attrs body]}]
-  (render-elements ctx body))
+  (render-elements ctx attrs body))
