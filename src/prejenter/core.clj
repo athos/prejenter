46ca5f60c2248eb7f22655ed43fb4b82b4a65a02
OHTA Shogo <athos0220@gmail.com>
2018-05-29 14:43:02 +0900

    Enable agent to reload slides

--- src/prejenter/core.clj
+++ src/prejenter/core.clj
@@ -1,37 +1,53 @@
 (ns prejenter.core
+  (:refer-clojure :exclude [find-var])
   (:require [prejenter.agent :as agent]
             [prejenter.agent.images :as images]))
 
-(def ^:private current-agent (atom nil))
+(def ^:private state (atom {}))
 
-(defn- find-name [ns name]
-  (let [kname (keyword (clojure.core/name name))]
-    (or (some->> (vals (ns-publics ns))
-                 (filter #(get (meta %) kname))
-                 first
-                 deref)
-        (some-> (ns-resolve ns name) deref))))
+(defn current-agent []
+  (:current-agent @state))
 
-(defn start! [& {:keys [ns context slides agent]}]
+(defn- set-current-agent! [agent]
+  (swap! state assoc :current-agent agent))
+
+(defn- find-var [ns name default]
+  (or (when ns
+        (let [kname (keyword (clojure.core/name name))]
+          (or (some->> (vals (ns-publics ns))
+                       (filter #(get (meta %) kname))
+                       first)
+              (ns-resolve ns name))))
+      default))
+
+(defn- make-agent [{:keys [ns context slides]}]
   (when ns
     (require ns :reload))
-  (let [context (or (some-> ns (find-name 'context))
-                    context)
-        slides (or (some-> ns (find-name 'slides))
-                   slides)
-        agent (or agent (images/make-images-agent context slides))]
-    (reset! current-agent agent)
+  (let [context (find-var ns 'context context)
+        slides (find-var ns 'slides slides)]
+    (images/make-images-agent @context @slides)))
+
+(defn start! [& {:as opts}]
+  (swap! state assoc :prev-opts opts)
+  (let [agent (make-agent opts)]
+    (set-current-agent! agent)
     (agent/start! agent)))
+
+(defn reload! []
+  (let [page (agent/current-page (current-agent))
+        agent (make-agent (:prev-opts @state))]
+    (set-current-agent! agent)
+    (agent/goto-page! agent page)))
 
 (defn next!
   ([] (next! 1))
   ([n]
-   (agent/next-page! @current-agent n)))
+   (agent/next-page! (current-agent) n)))
 
 (defn prev!
   ([] (prev! 1))
   ([n]
-   (agent/prev-page! @current-agent n)))
+   (agent/prev-page! (current-agent) n)))
 
 (defn goto! [page]
-  (agent/prev-page! @current-agent page))
+  (agent/prev-page! (current-agent) page))
